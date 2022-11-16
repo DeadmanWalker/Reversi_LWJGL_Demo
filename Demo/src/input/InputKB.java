1b74243a986de4974a3e33c01cfed2af0666178c
The Fool <minerender255@gmail.com>
2022-11-16 08:16:43 +0700

    Add click and release to InputKB and InputMouse

--- Demo/src/input/InputKB.java
+++ Demo/src/input/InputKB.java
@@ -4,10 +4,13 @@ import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWKeyCallback;
 
 public class InputKB extends GLFWKeyCallback{
-	
+	public static boolean[] click = new boolean[1000];
+	public static boolean[] release = new boolean[1000];
 	public static boolean[] keys = new boolean[1000];
 	@Override
 	public void invoke(long window, int key, int scancode, int action, int mods) {
+		click[key] = action != GLFW.GLFW_RELEASE && !keys[key];
+		release[key] = action == GLFW.GLFW_RELEASE && keys[key];
 		keys[key] = action != GLFW.GLFW_RELEASE;
 	}
 
--- Demo/src/input/InputMouse.java
+++ Demo/src/input/InputMouse.java
@@ -4,12 +4,13 @@ import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
 public class InputMouse extends GLFWMouseButtonCallback{
-	
+	public static boolean[] click = new boolean[100];
+	public static boolean[] release = new boolean[100];
 	public static boolean[] buttons = new boolean[100];
 	@Override
 	public void invoke(long window, int button, int action, int mods) {
+		click[button] = action != GLFW.GLFW_RELEASE && !buttons[button];
+		release[button] = action == GLFW.GLFW_RELEASE && buttons[button];
 		buttons[button] = action != GLFW.GLFW_RELEASE;
 	}
-	
-	
 }
